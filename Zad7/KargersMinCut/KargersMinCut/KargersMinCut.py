af127e2111d485639b4f5ec70bdba6968aefd417
Rafal <rafal.matuszak92@gmail.com>
2018-06-03 11:36:13 +0200

    Minor chages to main code. Add example graph.

--- Zad7/KargersMinCut/KargersMinCut/KargersMinCut.py
+++ Zad7/KargersMinCut/KargersMinCut/KargersMinCut.py
@@ -1,11 +1,11 @@
 import random
 import math
 
-#function for loading graph from text file
+#zaladowanie grafu z pliku tekstowego
 def load_graph():
-    return {int(line.rstrip().split()[0]): [int(i) for i in line.rstrip().split()[1:]] for line in open("source3.txt")}    
+    return {int(line.rstrip().split()[0]): [int(i) for i in line.rstrip().split()[1:]] for line in open("source.txt")}    
 
-#printing graph as combinations of key:value
+#drukowanie grafu w postaci wierzcholek:polaczone z nim wierzcholki
 def print_graph(graph):
     for k,v in graph.items():
         print(k)
@@ -14,25 +14,26 @@ def print_graph(graph):
 def contract_edge(edge):
     global graph
 
-    #merging verticies from cutted edges
+    #laczenie wierzcholkow z odcietych krawedzi
     v1l = graph[edge[0]]
     v1l.extend(graph[edge[1]])
     del graph[edge[1]]
 
-    #replace all occurnces of v2 value with v1
+    #zastapienie wszystkich wystapien v2 przez v1
     for k,l in graph.items():
         graph[k] = [edge[0] if x == edge[1] else x for x in graph[k]]
     
-    # Remove all edges of v1 to itself(loops)
+    #usuniecie petli wierzcholka v1 do samego siebie (cech multigrafu)
     graph[edge[0]] = [x for x in graph[edge[0]] if x != edge[0]]
 
 def get_random_edge():
+    #wylosowanie dwoch krawedzi v1 i v2, bazujac na polaczeniach od v1
     v1 = list(graph.keys()) [random.randint(0,len(graph)-1)]
     v2 = graph[v1] [random.randint(0,len(graph[v1])-1)]
     return (v1,v2)
 
-minList = []
 
+minList = []
 globalEdges = {}
 
 for x in range(0,20):
@@ -49,6 +50,9 @@ for x in range(0,20):
     minList.append(minlistel)
     globalEdges[x] = edges
 
-minimum = min(minList)
-print(minimum)
-print(globalEdges[minimum])
+    minimum = min(minList)
+    
+#drukowanie rezultatow
+print("Liczba powtorzen algorytmu:",20)
+print("Poszukiwana minimalna liczba ciec:",minimum)
+print("Dokonane ciecia (usuwanie krawedzie)",globalEdges[minimum])
